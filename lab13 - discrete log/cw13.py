8533114da0a5f9d35a3242841452c5bacebec839
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2022-06-08 22:31:36 +0200

    cw 13

--- lab13 - discrete log/cw13.py
+++ lab13 - discrete log/cw13.py
@@ -11,62 +11,55 @@ def ged(a, b):
 
 
 def calculate_dl(a, n):
-    r = 2
+    r = 1
+    MAX = 100
+
     while a ** r % n == 1:
         r += 1
+        if r > MAX:
+            return -1
 
     return r
 
 
 def discrete_log(N):
-    a = random.randint(1, N - 1)
+    MAX = 1000
 
-    # 1
-    value = ged(N, a)
-
-    # 2
-    if value > 1:
-        return a, 0, value
-
-    # 3
-    r = calculate_dl(a, N)
-
-    # 4
-    while r % 2 != 0:
+    while True:
+        MAX -= 1
+        if MAX == 0:
+            return 0,0,0
+        # 1
         a = random.randint(1, N - 1)
+
+        # 2
+        value = ged(N, a)
+        if value > 1:
+            return a, 0, value
+
+        # 3
         r = calculate_dl(a, N)
 
-    # 5
-    value_plus = ged(N, a ** (r / 2) + 1)
-    value_minus = ged(N, a ** (r / 2) - 1)
+        # 4
+        if r % 2 == 0:
+            # 5
+            value_plus = ged(N, a ** (r / 2) + 1)
+            value_minus = ged(N, a ** (r / 2) - 1)
+            if value_plus > 1:
+                return a, r, value_plus
+            if value_minus > 1:
+                return a, r, value_minus
 
-    # 6.1
-    if value_plus > 1:
-        return a, r, value_plus
-    if value_minus > 1:
-        return a, r, value_minus
-
-    # 6.2
-    while value_plus == 1 and value_minus == 1:
-        a = random.randint(1, N - 1)
-        r = calculate_dl(a, N)
-        while r % 2 != 0:
-            a = random.randint(1, N - 1)
-            r = calculate_dl(a, N)
-
-        value_plus = ged(N, a ** (r / 2) + 1)
-        value_minus = ged(N, a ** (r / 2) - 1)
-
-        if value_plus > 1:
-            return a, r, value_plus
-        if value_minus > 1:
-
-            return a, r, value_minus
 
 
 if __name__ == '__main__':
     print("Laboratoria 8 \n")
-    N = 5000
-    A, R, Value = discrete_log(N)
-
-    print("Znalzeiono poprawne rozwiązanie dla \nN: {}\na: {} \nr: {} \nNajwiekszy wspolny dzielnik: {}".format(N, A, R, Value))
+    N = [random.randint(100, 100000) for i in range(5)]
+    N = [12, 91, 57, 143, 1737, 1859, 13843, 988027] + N
+    for n in N:
+        A, R, Value = discrete_log(n)
+        if A == 0:
+            print("Nie znaleziono dzielnika")
+        else:
+            print("N: {} a: {} r: {} \n\tdzielnik: {}\n".format(n, A, R, Value))
+        input()
